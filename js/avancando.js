8caa55264ee4f2837dcbbcfbe7b2c638db46e050
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2023-10-10 17:20:12 -0300

    second commit

---
+++ js/avancando.js
@@ -0,0 +1,83 @@
+//Avançando em Javascript
+
+//Manipulaçao de array 
+const frutas = ['Uva', "Melão", "Banana"];
+
+frutas.unshift('Manga'); //adiciona no começo 
+frutas.push('Laranja'); //adiciona no final
+console.log(frutas);
+
+frutas.shift(); //remove no inicio
+console.log(frutas);
+
+frutas.pop(); //remove no final
+console.log(frutas);
+
+//map, filter, reduce -> arrow function
+const numeros = [1, 2, 3, 4, 5, 6];
+
+//find => retorna o primeiro elemento do critério
+const numeroPar = numeros.find((num) => num % 2 === 0);
+console.log(numeroPar);
+
+//filter => retorna todos os elementos que batem com o critério
+const numerosPares = numeros.filter((num) => num % 2 === 0);
+console.log(numerosPares);
+
+//Manipulaçao de strings
+const frase = " Olá mundo!   ";
+const palavras = frase.trim();  //trim remove o espaço
+console.log(palavras);
+
+const frase2 = "JavaScript é incrível!";
+console.log(frase2.startsWith("Java"));  //startsWith reconhece a palavra no início aponta como true
+console.log(frase2.endsWith("!")); //endsWith reconhece a palavra no final aponta como true
+
+
+//Exceções e tratamentos de erros
+const idade = 15;
+
+// if(idade < 18) {
+//   throw new Error("Precisa ter ao menos 18 anos de idade!");
+// }
+
+try {
+  const idade = 15;
+
+  if(idade < 18) {
+    throw new Error("Precisa ter ao menos 18 anos de idade!");
+  }
+  
+} catch (error) {
+  console.log(error);
+}
+
+console.log('Continuando o programa');
+
+
+//Callback => funçao
+function comprimentar(nome, callback) {
+  console.log("Olá, " + nome);
+  callback();
+}
+
+function mostrarSaudacao(){
+  console.log("Como está?");
+}
+
+comprimentar("Maria", mostrarSaudacao);
+
+
+//Callback aqui tbm
+comprimentar("Maria", function(){
+  console.log("Tudo bem?");
+});
+
+
+//settimeout => dps um tempo exeuta algo, uma vez 
+function mostrarMensagem(){
+  console.log('Essa msg será exibida em 3 min.');
+}
+
+//1000ms = 1s
+setTimeout(mostrarMensagem, 3000);
