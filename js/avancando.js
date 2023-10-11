903bf0ff2da94035cad8c47ba194b6156f3bcffb
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2023-10-11 09:29:31 -0300

    avancando

--- js/avancando.js
+++ js/avancando.js
@@ -81,3 +81,97 @@ function mostrarMensagem(){
 
 //1000ms = 1s
 setTimeout(mostrarMensagem, 3000);
+
+//outra meio de usar 
+setTimeout(function(){
+  console.log('oi');
+}, 3000);
+
+
+//Promises
+const promessa = new Promise((resolve, reject) => {
+  const condicao = true;
+
+  if(condicao){
+    resolve('Verdadeira');
+  } else {
+    reject('Falsa');
+  }
+});
+
+promessa.then((mensagem) => {
+  console.log(mensagem);
+})
+.catch((err) => {
+  console.log(err);
+});
+
+
+//Bibliotecas feitas que são "promise based"
+
+const promesa1 = Promise.resolve(3);
+const promesa2 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 2500, "testando");
+});
+
+Promise.all([promesa1, promesa2]).then((valores) => console.log(valores));
+
+
+//Async Await
+async function obterValor() {
+  const promessa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Valor obtido"), 2000);
+  });
+
+  const valor = await promessa;
+
+  console.log(valor);
+}
+
+obterValor();
+
+
+//Async Await com opção obter valor com erro -- USAR ESSE NO LUGAR DE PROMISE
+async function obterValorComErro() {
+ try {
+  const promessa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Valor com erro"), 2000);
+  });
+
+  const valor = await promessa;
+
+  console.log(valor);
+
+ } catch (error) {
+  console.log(error);
+ }
+}
+
+obterValorComErro();
+
+
+//JSON (JavaScript Object Notation)
+//{nome: "Maria"} - Objeto em js 
+//{"nome": "Maria"} - JSON
+//Padroniza a comunicação 
+//front-end e back-end em uma só linguagem 
+
+const objeto = { nome: "João", idade: 30  };  //objeto
+
+//transformando objeto em json 
+const jsonString = JSON.stringify(objeto);
+
+console.log(jsonString);
+console.log(typeof jsonString);
+
+// ------
+//jSON
+const json = '{ "nome": "Maria", "idade": 20 }';
+
+//transformando json em objeto
+const objeto2 = JSON.parse(json);
+
+console.log(objeto2);
+console.log(typeof objeto2);
+
+
---
+++ js/avancando_exercicios.js
@@ -0,0 +1,6 @@
+// Exercício de Manipulação de Array
+
+// Crie uma lista de compras que é inicialmente vazia.
+// Adicione 5 itens à lista usando push().
+// Agora, remova o primeiro item da lista usando shift().
+// Imprima a lista final no console.
